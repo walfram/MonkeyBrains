83bd17cb35571f4db46e1ba6cb352928c3285cc1
walfram <jevgeni.kalinin@gmail.com>
2024-04-24 20:44:45 +0300

    added FleeDemo

---
+++ demos-steering-behaviors/src/main/java/demos/FleeDemo.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ * <p>
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are
+ * met:
+ * <p>
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * <p>
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * <p>
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package demos;
+
+import com.jme3.ai.agents.Agent;
+import com.jme3.ai.agents.behaviors.npc.steering.BalancedCompoundSteeringBehavior;
+import com.jme3.ai.agents.behaviors.npc.steering.SeekBehavior;
+import com.jme3.ai.agents.behaviors.npc.SimpleMainBehavior;
+import com.jme3.ai.agents.behaviors.npc.steering.CompoundSteeringBehavior;
+import com.jme3.ai.agents.behaviors.npc.steering.FleeBehavior;
+import com.jme3.ai.agents.behaviors.npc.steering.RelativeWanderBehavior;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.system.AppSettings;
+
+/**
+ * Flee demo
+ *
+ * @author Jesús Martín Berlanga
+ * @version 2.0.1
+ */
+public class FleeDemo extends BasicDemo {
+
+  public static void main(String[] args) {
+    AppSettings settings = new AppSettings(true);
+    settings.setResolution(1600, 800);
+
+    FleeDemo app = new FleeDemo();
+    app.setSettings(settings);
+    app.setShowSettings(false);
+
+    app.start();
+  }
+
+  @Override
+  public void simpleInitApp() {
+    this.steerControl = new CustomSteerControl(11, 15);
+    this.steerControl.setCameraSettings(getCamera());
+    this.steerControl.setFlyCameraSettings(getFlyByCamera());
+
+    //defining rootNode for brainsAppState processing
+    brainsAppState.setApp(this);
+    brainsAppState.setGameControl(this.steerControl);
+
+    Vector3f[] spawnArea = null;
+    this.numberNeighbours = 20;
+
+    Agent target = this.createBoid("Target", this.targetColor, 0.1f);
+
+    brainsAppState.addAgent(target); //Add the target to the brainsAppState
+    rootNode.attachChild(target.getSpatial());
+    brainsAppState.getGameControl().spawn(target, new Vector3f(
+        (FastMath.nextRandomFloat() - 0.5f) * 5,
+        (FastMath.nextRandomFloat() - 0.5f) * 5,
+        (FastMath.nextRandomFloat() - 0.5f) * 5));
+    this.setStats(
+        target,
+        this.targetMoveSpeed,
+        this.targetRotationSpeed,
+        this.targetMass,
+        this.targetMaxForce);
+
+    Agent[] neighbours = new Agent[this.numberNeighbours];
+
+    for (int i = 0; i < this.numberNeighbours; i++) {
+      neighbours[i] = this.createBoid("Neighbour " + i, this.neighboursColor, 0.1f);
+      brainsAppState.addAgent(neighbours[i]); //Add the neighbours to the brainsAppState
+      rootNode.attachChild(neighbours[i].getSpatial());
+      this.setStats(
+          neighbours[i],
+          this.neighboursMoveSpeed,
+          this.neighboursRotationSpeed,
+          this.neighboursMass,
+          this.neighboursMaxForce);
+      brainsAppState.getGameControl().spawn(neighbours[i], spawnArea);
+    }
+
+    for (int i = 0; i < neighbours.length; i++) {
+      SimpleMainBehavior neighboursMainBehavior = new SimpleMainBehavior();
+      neighboursMainBehavior.setAgent(neighbours[i]);
+
+      CompoundSteeringBehavior compound = new CompoundSteeringBehavior();
+      compound.setAgent(neighbours[i]);
+
+      RelativeWanderBehavior wander = new RelativeWanderBehavior(10, 10, 10, 0.25f);
+      wander.setAgent(neighbours[i]);
+      wander.setupStrengthControl(0.37f);
+      
+      compound.addSteerBehavior(wander);
+      compound.addSteerBehavior(new SeekBehavior(neighbours[i], target));
+      
+      neighboursMainBehavior.addBehavior(compound);
+      neighbours[i].setMainBehavior(neighboursMainBehavior);
+    }
+
+    SimpleMainBehavior evaderMainBehavior = new SimpleMainBehavior();
+    evaderMainBehavior.setAgent(target);
+    
+    BalancedCompoundSteeringBehavior balancedCompoundSteeringBehavior = new BalancedCompoundSteeringBehavior();
+    balancedCompoundSteeringBehavior.setAgent(target);
+
+    for (int i = 0; i < neighbours.length; i++) {
+      FleeBehavior flee = new FleeBehavior(neighbours[i]);
+      balancedCompoundSteeringBehavior.addSteerBehavior(flee);
+    }
+    evaderMainBehavior.addBehavior(balancedCompoundSteeringBehavior);
+    target.setMainBehavior(evaderMainBehavior);
+
+    brainsAppState.start();
+  }
+
+  @Override
+  public void simpleUpdate(float tpf) {
+    brainsAppState.update(tpf);
+  }
+}
