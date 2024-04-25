815ad06b7c912c4286a8038a5aca7879a80bed27
walfram <jevgeni.kalinin@gmail.com>
2024-04-25 12:46:45 +0300

    added RelativeWanderDemo

---
+++ demos-steering-behaviors/src/main/java/demos/RelativeWanderDemo.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package demos;
+
+import com.jme3.ai.agents.Agent;
+import com.jme3.ai.agents.behaviors.npc.SimpleMainBehavior;
+import com.jme3.ai.agents.behaviors.npc.steering.RelativeWanderBehavior;
+import com.jme3.math.Vector3f;
+import com.jme3.system.AppSettings;
+
+/**
+ * Relative Wander Demo
+ *
+ * @author Jesús Martín Berlanga
+ * @version 1.0.1
+ */
+public class RelativeWanderDemo extends BasicDemo {
+
+    public static void main(String[] args) {
+        AppSettings settings = new AppSettings(true);
+        settings.setResolution(1600, 800);
+        
+        RelativeWanderDemo app = new RelativeWanderDemo();
+        app.setSettings(settings);
+        app.setShowSettings(false);
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        this.steerControl = new CustomSteerControl(9, 1);
+        this.steerControl.setCameraSettings(getCamera());
+        this.steerControl.setFlyCameraSettings(getFlyByCamera());
+
+        //defining rootNode for brainsAppState processing
+        brainsAppState.setApp(this);
+        brainsAppState.setGameControl(new CustomSteerControl(5f));
+
+        Agent target = this.createBoid("Target", this.targetColor, 0.11f);
+
+        brainsAppState.addAgent(target); //Add the target to the brainsAppState
+        rootNode.attachChild(target.getSpatial());
+        
+        brainsAppState.getGameControl().spawn(target, Vector3f.ZERO);
+        this.setStats(
+                target,
+                this.targetMoveSpeed,
+                this.targetRotationSpeed,
+                this.targetMass,
+                this.targetMaxForce);
+
+        SimpleMainBehavior targetMainBehavior = new SimpleMainBehavior();
+        RelativeWanderBehavior targetMoveBehavior = new RelativeWanderBehavior(10, 10, 10, 0.1f);
+        targetMoveBehavior.setAgent(target);
+        targetMoveBehavior.setTimeInterval(0.47f);
+        targetMainBehavior.addBehavior(targetMoveBehavior);
+        target.setMainBehavior(targetMainBehavior);
+
+        brainsAppState.start();
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        brainsAppState.update(tpf);
+    }
+}
